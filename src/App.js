504fa256febd0d0e0f16acd78725cc0317352090
jjcraft <jjc0046@mix.wvu.edu>
2023-10-30 14:19:28 -0400

    Added navbar and card components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+      <div className="App">
           <h1>CS 230L</h1>
           <h2>Section - 002</h2>
           <p> WVU ID: 800312805</p>
           <p>Hi, I am John Craft</p>
+          <NavBar />
+          <Card />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,6 +11,37 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+function NavBar() {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#">Navbar</Navbar.Brand>
+            </Container>
+        </Navbar>
+    );
+}
+
+function Card() {
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export {
+    NavBar, Card
+};
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
