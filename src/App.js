267531791a34c9ef1d5a1c673a212dfe140506f6
jjcraft <jjc0046@mix.wvu.edu>
2023-10-30 16:59:39 -0400

    navbar and card components added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Card from './Card';
+
 
 function App() {
-  return (
-      <div className="App">
-          <h1>CS 230L</h1>
-          <h2>Section - 002</h2>
-          <p> WVU ID: 800312805</p>
-          <p>Hi, I am John Craft</p>
-          <NavBar />
-          <Card />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <h1>CS 230L</h1>
+            <h2>Section - 002</h2>
+            <p>WVU ID: 800312805</p>
+            <p>Hi, I am John Craft</p>
+            <Card />
+        </div>
+    );
 }
 
 export default App;
